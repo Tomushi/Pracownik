990bb93b1c026a2374f1f193a777ea99de8f15c1
Tomek <107033433+Tomushi@users.noreply.github.com>
2024-10-02 17:57:56 +0200

    Added bool commande

--- Pracownik/Program.cs
+++ Pracownik/Program.cs
@@ -1,3 +1,46 @@
-﻿
+﻿using System;
 
-Console.WriteLine("Witaj w systemie dodawania pracownikow");
+namespace Pracownik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Witaj w systemie dodawania pracownikow");
+         
+            Console.WriteLine("Podaj imie pracownika");
+            
+            string imie = Console.ReadLine();
+            
+            Console.WriteLine("Podaj nazwisko pracownika");
+            
+            string nazwisko = Console.ReadLine();
+            
+            Console.WriteLine("Podaj wiek pracownika");
+            
+            int wiek = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Płeć pracownika");
+
+            string plec = "";
+            bool validPlec = false;
+
+            while (!validPlec)
+            {
+                Console.WriteLine("Podaj płeć pracownika (K/M)");
+                plec = Console.ReadLine();
+                if (plec == "K" || plec == "M")
+                {
+                    validPlec = true;
+                }
+                else
+                {
+                    Console.WriteLine("Podano nieprawidłową płeć");
+                }
+            }
+
+            Console.WriteLine("Podaj PESEL pracownika");
+            
+        }
+    }
+}
